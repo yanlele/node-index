985a278a534a8219f94beb246fa5d5e359db71db
yanlele <331393627@qq.com>
2017-12-27 19:32:13 +0800

    es6 变量的解构赋值

---
+++ book/ES6&7/2、变量的解构赋值/index.js
@@ -0,0 +1,22 @@
+/*//3.1  数组的结构赋值
+
+const [a,b,c]=[1,2,3];
+console.log({a,b,c});*/
+
+/*
+//3.2  对象的解构赋值
+var obj = {
+    p: [
+        'Hello',
+        { y: 'World' }
+    ]
+};
+var { p: [x, { y }] } = obj;
+x // "Hello"
+y // "World"*/
+
+
+
+
+
+
