835acba8458247f10b1fd42d990970af94895a78
yanlele <331393627@qq.com>
2018-01-16 16:00:24 +0800

    一个koa2的具体架构实例

---
+++ 18年/1月/12、koa2项目框架搭建/config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: '005_koa_demo',
+    USERNAME: 'yanle',
+    PASSWORD: '53693750',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
