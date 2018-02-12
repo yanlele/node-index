9ac48d48f99ebca58d9d75414d210f6d5979ac08
yanlele <331393627@qq.com>
2018-02-12 22:42:06 +0800

    path公共模块api

--- 18年/2月/3、node基础api/02、path_join.js
+++ 18年/2月/3、node基础api/02、path_join.js
@@ -1,2 +1,3 @@
 const path=require('path');
 console.log(path.join('/user','local','bin/'));
+console.log(path.join('/user','..local','bin/'));
---
+++ 18年/2月/3、node基础api/04、path_names.js
@@ -0,0 +1,7 @@
+const path=require('path');
+
+const filePath= '/user/local/bin/no.txt';
+
+console.log(path.basename(filePath));
+console.log(path.dirname(filePath));
+console.log(path.extname(filePath));
