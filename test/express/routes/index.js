6c6b76d131d1eee1f923916055fa27ba1dfcbb29
yanlele <331393627@qq.com>
2018-04-17 19:12:51 +0800

    [*] 改造一下express的router

--- test/express/routes/index.js
+++ test/express/routes/index.js
@@ -2,7 +2,12 @@ const users = require('./users');
 
 function router(app) {
     /* GET home page. */
-    app.use('/', users);
+    app.use('/', function (req, res, next) {
+        res.render('index',{
+            title: 'yanle express'
+        })
+    });
+    app.use('/user', users);
 }
 
 module.exports = router;
