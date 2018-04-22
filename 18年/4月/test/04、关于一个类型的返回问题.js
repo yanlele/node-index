c49bbb0c104c7c7861e1f1a5dc26fefc06fd2a2e
yanlele <331393627@qq.com>
2018-04-22 16:51:27 +0800

    [+]一个范围问题的测试

--- 18年/4月/test/04、关于一个类型的返回问题.js
+++ 18年/4月/test/04、关于一个类型的返回问题.js
@@ -1,2 +1,16 @@
-let arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9, '123'];
 console.log(typeof arr);
+//遍历方法1
+for(let item of arr) {
+    if(typeof item !== 'number') {
+        console.log(item);
+    }
+}
+console.log('==================');
+arr.forEach((item, index, arr) => {
+    if(typeof item !== 'number') {
+        console.log(item);
+        console.log(index);
+        console.log(arr);
+    }
+});
---
+++ 18年/4月/test/05、验证一个范围级别的问题.js
@@ -0,0 +1,6 @@
+let int = 6;
+if(1<int<5) {
+    console.log('1 - 5')
+} else  {
+    console.log('false')
+}
