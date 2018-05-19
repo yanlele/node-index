b1b27539f09ab2ec295f721eadce2be981642d25
yanlele <331393627@qq.com>
2018-05-19 22:45:12 +0800

    [+]统计重复算法研究

---
+++ 18年/5月/04、关于取重算法的研究/index.js
@@ -0,0 +1,12 @@
+let arr = [];
+for (let i = 0; i < 100; i++) {
+    arr.push(Math.floor(Math.random() * 10))
+}
+
+[1,1,2,2,3,4,5,5,4,4,4,4].map(function (item, index, list) {
+    list.map(function(listItem, listIndex) {
+        if(listIndex > index && item === listItem) {
+            console.log(item)
+        }
+    })
+});
