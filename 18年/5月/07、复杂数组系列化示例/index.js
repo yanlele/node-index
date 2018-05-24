364072e8b76a6bb475e9b7da96362fbd53877c6a
yanlele <331393627@qq.com>
2018-05-24 10:35:40 +0800

    [+]复杂数组系列化示例

--- 18年/5月/07、复杂数组系列化示例/index.js
+++ 18年/5月/07、复杂数组系列化示例/index.js
@@ -3,7 +3,7 @@ let arr = ['123-yanle', '234-lele', '345-yanlele'];
 console.log(arr);
 
 let newArray = [];
-arr.map(function(item, index) {
-     newArray.push(item.split('-')[1]);
+arr.map(function (item, index) {
+    newArray.push(item.split('-')[1]);
 });
 console.log(newArray.join(' | '));
