cb33cbd97126c6680ed51d3a0defd7245f5a5584
yanlele <331393627@qq.com>
2018-05-31 21:54:06 +0800

    [+]关于时间模块的moment使用

---
+++ 18年/6月/01、关于模块moment的使用/README.md
@@ -0,0 +1,13 @@
+#  关于时间模块moment的使用
+
+## Now 获取当前时间
+具体语法：           
+```javascript
+moment();
+// From 2.14.0 onward, also supported
+moment([]);
+moment({});
+```
+
+
+
---
+++ 18年/6月/01、关于模块moment的使用/index.js
@@ -0,0 +1,8 @@
+let moment = require('moment');
+
+let day1 = moment();
+let day2 = moment([]);
+let day3 = moment({});
+console.log(day1);
+console.log(day2);
+console.log(day3);
