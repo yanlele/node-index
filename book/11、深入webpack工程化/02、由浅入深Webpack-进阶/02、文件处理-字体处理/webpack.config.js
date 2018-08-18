ca08a1d686dae0b09a2e203f0f09fc4defc36af7
yanlele <331393627@qq.com>
2018-08-18 11:49:09 +0800

    [+]文件处理-字体处理

---
+++ book/11、深入webpack工程化/02、由浅入深Webpack-进阶/02、文件处理-字体处理/webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const Webpack = require('webpack');
+const PurifyCSS = require('purifycss-webpack');
+const glob = require('glob-all');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/app.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        publicPath: './dist/',
+        chunkFilename: '[name].bundle.js',              //动态打包文件名
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: {
+                        loader: 'style-loader',
+                        options: {
+                            singleton: true,
+                            transform: './css.transform.js'
+                        }
+                    },
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                // minimize: true,
+                                // modules: true,
+                                localIdentName: '[path][name]_[local]_[hash:base64:5]'
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ident: 'postcss',
+                                plugins: [
+                                    require('postcss-cssnext')()
+                                ]
+                            }
+                        },
+                        {
+                            loader: 'less-loader'
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024 * 10,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    // useRelativePath: true,
+                                    publicPath: '../img',
+                                    outputPath: 'img',
+                                }
+                            }
+                        }
+                    },
+                    {
+                        loader: 'img-loader'
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextWebpackPlugin({
+            filename:  (getPath) => {
+                return getPath('css/[name].min.css').replace('css/js', 'css');
+            },
+            allChunks: false
+        }),
+
+        new PurifyCSS({
+            paths: glob.sync([
+                path.join(__dirname, './*.html'),
+                path.join(__dirname, './src/*.js')
+            ]),
+        }),
+
+        new Webpack.optimize.UglifyJsPlugin()
+    ]
+};
