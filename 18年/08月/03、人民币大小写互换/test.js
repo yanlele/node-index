5d14ad2f67c3108de9160ae6f38d978fb54c5028
yanlele <331393627@qq.com>
2018-08-20 17:52:55 +0800

    [+]人民币大小写互换

---
+++ 18年/08月/03、人民币大小写互换/test.js
@@ -0,0 +1,7 @@
+const rmbHelper = require('./rmbHelper');
+
+let rmb = 123764.123;
+let RMB = rmbHelper.convert(rmb);
+console.log(RMB);
+// let rmbNum = rmbHelper.rmbToNum(RMB);
+// console.log(rmbNum);
