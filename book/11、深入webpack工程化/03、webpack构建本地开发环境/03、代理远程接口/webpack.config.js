d10bd20f1750454f02d8cdb0322a64aaae492a17
yanlele <331393627@qq.com>
2018-08-25 23:27:05 +0800

    [+]代理代码提交

---
+++ book/11、深入webpack工程化/03、webpack构建本地开发环境/03、代理远程接口/webpack.config.js
@@ -0,0 +1,174 @@
+const path = require('path');
+const Webpack = require('webpack');
+const PurifyCSS = require('purifycss-webpack');
+const glob = require('glob-all');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const HtmlInlineChunkPlugin = require('html-webpack-inline-chunk-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const Jarvis = require('webpack-jarvis');
+
+module.exports = {
+    entry: {
+        app: './src/app.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.[hash:5].js',
+        publicPath: '/',
+        chunkFilename: '[name].bundle.[hash:5].js',              //动态打包文件名
+    },
+
+    devServer: {
+        port: 3000,
+        proxy: {
+            '/':{
+                target: 'http://localhost:3002',
+                changeOrigin: true,
+                logLevel: 'debug',
+                pathRewrite: {
+                    '':'/api/'
+                }
+            }
+        }
+    },
+
+    resolve: {
+        alias: {
+            jquery$: path.resolve(__dirname, 'src/lib/jquery.min.js')                  // 之所以要用jquery$ ,表示这是一个文件而已；
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: {
+                        loader: 'style-loader',
+                        options: {
+                            singleton: true,
+                            transform: './css.transform.js'
+                        }
+                    },
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                // minimize: true,
+                                // modules: true,
+                                localIdentName: '[path][name]_[local]_[hash:base64:5]'
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ident: 'postcss',
+                                plugins: [
+                                    require('postcss-cssnext')()
+                                ]
+                            }
+                        },
+                        {
+                            loader: 'less-loader'
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024 * 2,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    // useRelativePath: true,
+                                    // publicPath: '../img',
+                                    outputPath: './img/',
+                                }
+                            }
+                        }
+                    },
+                    {
+                        loader: 'img-loader'
+                    }
+                ]
+            },
+            {
+                test: path.resolve(__dirname, 'src/app.js'),
+                use: [
+                    {
+                        loader: 'imports-loader',
+                        options: {
+                            $: 'jquery'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            attrs: ['img:src', 'img:data-src']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextWebpackPlugin({
+            filename: (getPath) => {
+                return getPath('css/[name].min.[hash:5].css').replace('css/js', 'css');
+            },
+            allChunks: false
+        }),
+
+        new PurifyCSS({
+            paths: glob.sync([
+                path.join(__dirname, './*.html'),
+                path.join(__dirname, './src/*.js')
+            ]),
+        }),
+
+        new Webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest'
+        }),
+
+        new HtmlInlineChunkPlugin({
+            inlineChunks: ['manifest']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html',
+            // chunks: ['app'],
+            minify: {
+                collapseWhitespace: false                //祛除空格
+            }
+        }),
+
+        new Webpack.optimize.UglifyJsPlugin(),
+
+        new CleanWebpackPlugin(['dist']),
+
+        new Jarvis({
+            port: 3001
+        })
+    ]
+};
