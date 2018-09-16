0cecf809bcd420026061241451c0dd4c1497acf3
yanlele <331393627@qq.com>
2018-09-17 00:35:12 +0800

    [+]提交原始的koalog文件

--- 18年/09月/04、对日志监控系统的研究(bunyan)/koaLog.js
+++ 18年/09月/04、对日志监控系统的研究(bunyan)/koaLog.js
@@ -47,7 +47,7 @@ const defaultPath = path.resolve(__dirname, 'logs');
 const applicationName = '我是程序日志';
 
 function writeLogFile(options) {
-    let filePath = path.resolve(defaultPath, applicationName + '_' + options.type + '.log');
+    let filePath = path.resolve(options.defaultPath, applicationName.applicationName + '_' + options.type + '.log');
     fs.writeFile(filePath, options.writeMessage, {
         'flag': 'a'
     }, function (err) {
@@ -55,42 +55,60 @@ function writeLogFile(options) {
     })
 }
 
+
+
+/**
+ *
+ * @param options
+ * @returns {log} 必须要包含 defaultPath 和 applicationName
+ * @constructor
+ */
 function KoaLog(options) {
-    const defaultOptions = {
-        writeMessage: '',
-        type: '',
-
+    let defaultOptions = {
+        defaultPath: __dirname
     };
+    // get package.json name
+    try {
+        const pkg = require('./package.json');
+        if(pkg && pkg.name) {
+            defaultOptions.applicationName = pkg.name;
+        }
+    } catch(e) {
+        defaultOptions.applicationName = ''
+    }
     options = Object.assign(defaultOptions, options);
-    this.message = options.writeMessage;
-    this.type = options.type;
 
+    logTypeList.forEach(function (logType) {
+        logType.type = function () {
+            let args = Array.prototype.slice.call(arguments, 0);
+            let writeMessage = `[${now} - ${logType.type}] ${args}` + '\n';
+            if (logType.icon) args = [logType.icon].concat(args);
+            let message = `[${now} - ${logType.type}] ${chalk[logType.color].apply(global.console, args)}`;
+            global.console.log(message);
+            if (logType.type === 'info' || logType.type === 'error' || logType === 'fate') {
+                // 如果是这三种情况的日志，就需要输入 到日志文件夹
 
+                fs.exists(defaultPath, function (filePathMsg) {
+                    if (filePathMsg) {
+                        // 文件夹存在 直接写入日志
+                        writeLogFile(Object.assign(options, {type: logType.type, writeMessage}));
+                    } else {
+                        new Error('文件夹不存在');
+                    }
+                })
+            }
+        };
+    });
 
     function log(ctx, next) {
-        logTypeList.forEach(function (logType) {
-            exports[logType.type] = function () {
-                let args = Array.prototype.slice.call(arguments, 0);
-                let writeMessage = `[${now} - ${logType.type}] ${args}` + '\n';
-                if (logType.icon) args = [logType.icon].concat(args);
-                let message = `[${now} - ${logType.type}] ${chalk[logType.color].apply(global.console, args)}`;
-                global.console.log(message);
-                if (logType.type === 'info' || logType.type === 'error' || logType === 'fate') {
-                    // 如果是这三种情况的日志，就需要输入 到日志文件夹
-
-                    fs.exists(defaultPath, function (filePathMsg) {
-                        if (filePathMsg) {
-                            // 文件夹存在 直接写入日志
-                            writeLogFile({type: logType.type, writeMessage});
-                        } else {
-                            new Error('文件夹不存在');
-                        }
-                    })
-                }
-            };
-        });
+        console.log(this);
     }
     return log;
 }
+
+KoaLog({
+    defaultPath: path.resolve(__dirname, 'logs'),
+    applicationName: 'app'
+})();
 
 module.exports = KoaLog;
