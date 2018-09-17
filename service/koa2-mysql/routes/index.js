b2ab07085f8470333a49a9e2d9b5ef04e1b098d7
yanlele <331393627@qq.com>
2018-09-17 20:28:50 +0800

    [+]添加一个koa测试实例

--- service/koa2-mysql/routes/index.js
+++ service/koa2-mysql/routes/index.js
@@ -6,9 +6,11 @@ const router = require('koa-router')();
 
 const home = require('./home');
 const user=require('./users');
+const test = require('./test');
 
-router.use('/', home.routes(), home.allowedMethods());
+router.use('/test', test.routes(), test.allowedMethods());
 router.use('/user',user.routes(),user.allowedMethods());
+router.use('/test', home.routes(), test.allowedMethods());
 
 module.exports = router;
 
