24010d20880e4a54074afd1af2a03400720863f7
yanlele <331393627@qq.com>
2018-10-16 14:50:58 +0800

    [+]添加一个数组undefined的测试

---
+++ 18年/10月/test/03、数组undefined.js
@@ -0,0 +1,14 @@
+/**
+ * create by yanlele
+ * create time 2018-10-16 14:46
+ */
+
+
+let arr = [1,2,3,4,5,6 , undefined, 7,8,9];
+
+console.log(arr);
+
+arr.map(function (item, index) {
+    console.log('item: ', item);
+    console.log('index: ', index);
+});
