71f4b2165ce8f29659f2818099c08a299ce6389b
yanlele <331393627@qq.com>
2018-11-09 17:26:23 +0800

    [+]基础知识点儿 - 继承的研究完结

--- book/05、基础知识点专题/test/01-07、继承.js
+++ book/05、基础知识点专题/test/01-07、继承.js
@@ -4,7 +4,7 @@
  */
 
 
-function Parent(name) {
+/*function Parent(name) {
     this.name = name;
     this.getName = function () {
         console.log(this.name);
@@ -18,4 +18,22 @@ function Child(name) {
 
 let child = new Child('yanle');
 child.getName();
-console.log(child.type);
+console.log(child.type);*/
+
+function Parent5() {
+    this.name = 'parent5';
+    this.play = [1, 2, 3];
+}
+
+function Child5() {
+    Parent5.call(this);
+    this.type = 'child5'
+}
+
+Child5.prototype = Object.create(Parent5.prototype);
+//这个时候虽然隔离了，但是constructor还是只想的Parent5的，因为constructor会一直向上找
+Child5.prototype.constructor=Child5;
+
+var s7=new Child5();
+console.log(s7 instanceof Child5,s7 instanceof Parent5);
+console.log(s7.constructor);
