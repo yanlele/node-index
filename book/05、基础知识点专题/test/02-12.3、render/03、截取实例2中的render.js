b8502a9aa13fa543f1fd684ed57f38d95919e976
yanlele <331393627@qq.com>
2018-11-18 22:52:21 +0800

    [+]整理codeRender

--- book/05、基础知识点专题/test/02-12.3、render/03、截取实例2中的render.js
+++ book/05、基础知识点专题/test/02-12.3、render/03、截取实例2中的render.js
@@ -6,23 +6,42 @@
 
 function codeRender() {
     with (this) {
-        return _c('div', {attrs: {"id": "app"}}, [_c('div', [_c('input', {
-            directives: [{
-                name: "model",
-                rawName: "v-model",
-                value: (title),
-                expression: "title"
-            }], domProps: {"value": (title)}, on: {
-                "input": function ($event) {
-                    if ($event.target.composing) return;
-                    title = $event.target.value
-                }
-            }
-        }), _v(" "), _c('br'), _v(" "), _c('button', {
-            attrs: {"id": "btn-submit"},
-            on: {"click": add}
-        }, [_v("submit")])]), _v(" "), _c('div', [_c('ul', {attrs: {"id": "ul-list"}}, _l((list), function (item) {
-            return _c('li', [_v(_s(item))])
-        }))])])
+        return _c('div',
+            {attrs: {"id": "app"}},
+            [
+                _c('div', [
+                    _c('input', {
+                        directives: [{
+                            name: "model",
+                            rawName: "v-model",
+                            value: (title),
+                            expression: "title"
+                        }],
+                        domProps: {"value": (title)},
+                        on: {
+                            "input": function ($event) {
+                                if ($event.target.composing) return;
+                                title = $event.target.value
+                            }
+                        }
+                    }),
+                    _v(" "),
+                    _c('br'),
+                    _v(" "),
+                    _c('button', {
+                            attrs: {"id": "btn-submit"},
+                            on: {"click": add}
+                        },
+                        [_v("submit")])]),
+
+                _v(" "),
+
+                _c('div', [
+                    _c('ul',
+                        {attrs: {"id": "ul-list"}},
+                        _l((list), function (item) {
+                            return _c('li', [_v(_s(item))])
+                        }))])
+            ])
     }
 }
