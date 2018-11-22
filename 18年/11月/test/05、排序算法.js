594610b64dac17b73519561e10b5287a570fe910
yanlele <331393627@qq.com>
2018-11-22 15:56:57 +0800

    [+]快速排序法

--- 18年/11月/test/05、排序算法.js
+++ 18年/11月/test/05、排序算法.js
@@ -3,11 +3,28 @@
  * create time 2018-11-22 14:47
  */
 
+let arrayNumber = [12,22,34,56,11,3,77,39,32];
+
 /*快速排序*/
 let quickSort = function (arr) {
     if (arr.length <= 1) {
         return arr;
     }
+    let pivotIndex = Math.floor(arr.length/2);
+    let pivot = arr.splice(pivotIndex, 1)[0];
 
-
+    let left = [];
+    let right = [];
+    for (let i =0; i<arr.length;i++) {
+        if(arr[i]<pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+    return quickSort(left).concat([pivot], quickSort(right));
 };
+
+console.log(quickSort(arrayNumber));
+
+/*选择排序*/
