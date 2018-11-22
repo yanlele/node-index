6f3c1dc73fce7b03f060189a8c830d6176aa8b56
yanlele <331393627@qq.com>
2018-11-22 16:16:39 +0800

    [+]选择排序

--- 18年/11月/test/05、排序算法.js
+++ 18年/11月/test/05、排序算法.js
@@ -3,20 +3,20 @@
  * create time 2018-11-22 14:47
  */
 
-let arrayNumber = [12,22,34,56,11,3,77,39,32];
+let arrayNumber = [12, 22, 34, 56, 11, 3, 77, 39, 32];
 
 /*快速排序*/
 let quickSort = function (arr) {
     if (arr.length <= 1) {
         return arr;
     }
-    let pivotIndex = Math.floor(arr.length/2);
+    let pivotIndex = Math.floor(arr.length / 2);
     let pivot = arr.splice(pivotIndex, 1)[0];
 
     let left = [];
     let right = [];
-    for (let i =0; i<arr.length;i++) {
-        if(arr[i]<pivot) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
             left.push(arr[i])
         } else {
             right.push(arr[i])
@@ -28,3 +28,23 @@ let quickSort = function (arr) {
 console.log(quickSort(arrayNumber));
 
 /*选择排序*/
+let selectionSort = function (arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let minIndex, temp;
+    for (let i = 0; i < arr.length - 1; i++) {
+        minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    return arr;
+};
+
+console.log(selectionSort(arrayNumber));
