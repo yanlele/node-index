0adae55fbe70e60a072bfa7ac3e9a07beda8f5ae
yanlele <331393627@qq.com>
2018-11-23 14:24:26 +0800

    [+]数组去重方法总结

---
+++ 18年/11月/test/06、数组去重算法.js
@@ -0,0 +1,45 @@
+/**
+ * create by yanlele
+ * create time 2018-11-23 12:36
+ */
+
+let arrNumber = [11, 11, 23, 4, 5, 6, 7, 88, 88, 11];
+
+let unique1 = function (arr) {
+    let n = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (n.indexOf(arr[i]) === -1) {
+            n.push(arr[i])
+        }
+    }
+    return n
+};
+
+
+let unique2 = function (arr) {
+    let n = {}, r = [], len = arr.length, val, type;
+    for (let i = 0; i < len; i++) {
+        val = arr[i];
+        type = typeof val;
+        if (!n[val]) {
+            n[val] = [type];
+            r.push(val);
+        } else if (n[val].indexOf(type) === -1) {
+            n[val].push(type);
+            r.push(val);
+        }
+    }
+    return r;
+};
+
+let unique3 = function (arr) {
+    let n = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.indexOf(arr[i]) === i) {
+            n.push(arr[i])
+        }
+    }
+    return n;
+};
+console.log(unique3(arrNumber));
+
