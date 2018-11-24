f75c621f9dd764dafaa8eae847ae5696be762b79
yanlele <331393627@qq.com>
2018-11-24 13:38:06 +0800

    [+]排序算法完成

--- 18年/11月/test/05、排序算法2.js
+++ 18年/11月/test/05、排序算法2.js
@@ -60,3 +60,20 @@ let selectionSort = function (arr) {
     return arr;
 };
 console.log(selectionSort(arrayNumber));
+
+
+let insertSort = function (arr) {
+    let len = arr.length;
+    let preIndex, current;
+    for (let i = 1; i < len; i++) {
+        preIndex = i - 1;
+        current = arr[i];
+        while (preIndex >= 0 && arr[preIndex] > current) {
+            arr[preIndex + 1] = arr[preIndex];
+            preIndex--;
+        }
+        arr[preIndex + 1] = current;
+    }
+    return arr;
+};
+console.log(insertSort(arrayNumber));
