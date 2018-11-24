6739fc9db43e3d7bd708e66f4ea6bb72ba41a949
yanlele <331393627@qq.com>
2018-11-24 12:30:25 +0800

    [+]继承的实现

--- 18年/11月/test/03、继承的实现2.js
+++ 18年/11月/test/03、继承的实现2.js
@@ -1,28 +1,45 @@
-/**
- * create by yanlele
- * create time 2018-11-23 9:45
- */
-
-/*let Parent = function () {
+/*
+let Parent = function () {
     this.name = 'yanle';
     this.getName = function () {
-        console.log(this.name);
+        console.log(this.name)
     }
 };
+
 let Child = function () {
     Parent.call(this);
-    this.type = 'child';
-};*/
+    this.type = 'child'
+};
+
+*/
 
 
 /*let Parent = function () {
     this.name = 'yanle';
     this.getName = function () {
-        console.log(this.name);
+        console.log(this.name)
     }
 };
+
 let Child = function () {
-    this.type = 'child';
+    this.type = 'child'
+};
+
+Child.prototype = new Parent();
+let child = new Child();
+child.getName()*/
+
+
+/*let Parent = function () {
+    this.name = 'yanle';
+    this.getName = function () {
+        console.log(this.name)
+    }
+};
+
+let Child = function () {
+    Parent.call(this);
+    this.type = 'child'
 };
 Child.prototype = new Parent();*/
 
@@ -30,38 +47,29 @@ Child.prototype = new Parent();*/
 /*let Parent = function () {
     this.name = 'yanle';
     this.getName = function () {
-        console.log(this.name);
+        console.log(this.name)
     }
 };
+
 let Child = function () {
     Parent.call(this);
-    this.type = 'child';
-};
-Child.prototype = new Parent();*/
-
-
-/*let Parent = function () {
-    this.name = 'yanle';
-    this.getName = function () {
-        console.log(this.name);
-    }
-};
-let Child = function () {
-    this.type = 'child';
+    this.type = 'child'
 };
 Child.prototype = Parent.prototype;*/
 
-/*function Parent5() {
-    this.name = 'parent5';
-    this.play = [1, 2, 3];
-}
+/*let Parent = function () {
+    this.name = 'yanle';
+    this.getName = function () {
+        console.log(this.name)
+    }
+};
 
-function Child5() {
-    Parent5.call(this);
-    this.type = 'child5'
-}
+let Child = function () {
+    Parent.call(this);
+    this.type = 'child'
+};
+Child.prototype = Object.create(Parent.prototype);
+Child.prototype.constructor = Child;*/
 
-Child5.prototype = Object.create(Parent5.prototype);
-//这个时候虽然隔离了，但是constructor还是只想的Parent5的，因为constructor会一直向上找
-Child5.prototype.constructor=Child5;*/
+
 
