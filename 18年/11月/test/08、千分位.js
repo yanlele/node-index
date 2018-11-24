1b28c18d0946e27b4e72b4c412d5deff2dc02d0e
yanlele <331393627@qq.com>
2018-11-24 15:26:35 +0800

    [+]千分位

---
+++ 18年/11月/test/08、千分位.js
@@ -0,0 +1,13 @@
+/**
+ * create by yanle
+ * connect me 331393627@qq.com
+ * create time 2018-11-24 15:21
+ */
+
+
+
+let reg= /(\d{1,3})(?=(\d{3})+(?:$|\.))/g;
+let str = '12337487.123';
+
+let newStr =  str.replace(reg, '$1,');
+console.log(newStr);
