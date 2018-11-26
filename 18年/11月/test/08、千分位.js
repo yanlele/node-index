03bcf86534e20c9a8e51e6f4d8466f196dbdafb1
yanlele <331393627@qq.com>
2018-11-26 14:37:58 +0800

    [+]MDN 的解释是 bind()

--- 18年/11月/test/08、千分位.js
+++ 18年/11月/test/08、千分位.js
@@ -5,8 +5,16 @@
  */
 
 
+/*
 
 let reg= /(\d{1,3})(?=(\d{3})+(?:$|\.))/g;
+let str = '12337487.123';
+
+let newStr =  str.replace(reg, '$1,');
+console.log(newStr);*/
+
+
+let reg = /(\d{1,3})(?=(\d{3})+(?:$|.))/g
 let str = '12337487.123';
 
 let newStr =  str.replace(reg, '$1,');
