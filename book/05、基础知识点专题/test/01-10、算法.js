eec568121b3f09131479d78680b0e8406343de7f
yanlele <331393627@qq.com>
2018-11-27 23:27:12 +0800

    [+]实例算法的改善

---
+++ book/05、基础知识点专题/test/01-10、算法.js
@@ -0,0 +1,75 @@
+/**
+ * create by yanle
+ * connect me 331393627@qq.com
+ * create time 2018-11-27 23:18
+ */
+
+let arrNumber = [11,5, 23, 4,43, 6, 7, 88, 77, 66];
+var quickSort = function(arr) {
+    if (arr.length <= 1) { return arr; }
+    var pivotIndex = Math.floor(arr.length / 2);   //基准位置（理论上可任意选取）
+    var pivot = arr.splice(pivotIndex, 1)[0];  //基准数   splice 会改改变源数组！！！！！！！！
+    var left = [];
+    var right = [];
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat([pivot], quickSort(right));  //链接左数组、基准数构成的数组、右数组
+};
+console.log(quickSort(arrNumber.slice(0)));
+
+function selectionSort(arr) {
+    var len = arr.length;
+    var minIndex, temp;
+    for (var i = 0; i < len - 1; i++) {
+        minIndex = i;
+        for (var j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {     // 寻找最小的数
+                minIndex = j;                 // 将最小数的索引保存
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    return arr;
+}
+console.log(selectionSort(arrNumber));
+
+
+function insertionSort(arr) {
+    var len = arr.length;
+    var preIndex, current;
+    for (var i = 1; i < len; i++) {
+        preIndex = i - 1;
+        current = arr[i];
+        while(preIndex >= 0 && arr[preIndex] > current) {
+            arr[preIndex+1] = arr[preIndex];
+            preIndex--;
+        }
+        arr[preIndex+1] = current;
+    }
+    return arr;
+}
+console.log(insertionSort(arrNumber));
+
+
+
+function bubbleSort(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len - 1; i++) {
+        for (var j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+                var temp = arr[j+1];        // 元素交换
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(bubbleSort(arrNumber));
