1fc85ed66d04a32c487a9d2fce7ee1ac06835ae7
yanlele <331393627@qq.com>
2018-11-30 17:20:21 +0800

    [+]千分位

--- 18年/11月/test/08、千分位.js
+++ 18年/11月/test/08、千分位.js
@@ -14,7 +14,7 @@ let newStr =  str.replace(reg, '$1,');
 console.log(newStr);*/
 
 
-let reg = /(\d{1,3})(?=(\d{3})+(?:$|.))/g;
+let reg= /(\d{1,3})(?=(\d{3})+(?:$|\.))/g;
 let str = '12337487.123';
 
 let newStr =  str.replace(reg, '$1,');
