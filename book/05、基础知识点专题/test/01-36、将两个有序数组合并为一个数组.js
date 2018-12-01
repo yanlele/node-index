52f517d3833f83a34db9364fc88eeb19f36bc76e
yanlele <331393627@qq.com>
2018-12-01 23:03:06 +0800

    [+]一个简单的算法设计

--- book/05、基础知识点专题/test/01-36、将两个有序数组合并为一个数组.js
+++ book/05、基础知识点专题/test/01-36、将两个有序数组合并为一个数组.js
@@ -6,5 +6,28 @@
 
 let merge = function (left, right) {
     let leftIndex = 0, rightIndex = 0;
+    let result = [];
+    let leftLen = left.length;
+    let rightLen  = right.length;
+    let diffIndex = leftLen - rightLen;
+    let connectList;
+    if(diffIndex > 0) {
+        connectList = left.slice(rightLen);
+    } else {
+        connectList = right.slice(leftLen);
+    }
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex++])
+        } else {
+            result.push(right[rightIndex++])
+        }
+    }
+    // console.log(result);
+    result = result.concat(connectList);
+    return result;
+};
 
-}
+let left = [1, 4, 7, 8, 9, 10];
+let right = [2, 5];
+console.log(merge(left, right));
