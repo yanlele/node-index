d501309703028014eaf88db9c7107fe896eddb0b
yanlele <331393627@qq.com>
2018-12-01 17:08:29 +0800

    [+]提交一个希尔算法

--- book/05、基础知识点专题/test/01-10、算法.js
+++ book/05、基础知识点专题/test/01-10、算法.js
@@ -5,71 +5,93 @@
  */
 
 let arrNumber = [11,5, 23, 4,43, 6, 7, 88, 77, 66];
-let quickSort = function(arr) {
-    if (arr.length <= 1) { return arr; }
-    let pivotIndex = Math.floor(arr.length / 2);   //基准位置（理论上可任意选取）
-    let pivot = arr.splice(pivotIndex, 1)[0];  //基准数   splice 会改改变源数组！！！！！！！！
-    let left = [];
-    let right = [];
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-    return quickSort(left).concat([pivot], quickSort(right));  //链接左数组、基准数构成的数组、右数组
-};
-console.log(quickSort(arrNumber.slice(0)));
+// let quickSort = function(arr) {
+//     if (arr.length <= 1) { return arr; }
+//     let pivotIndex = Math.floor(arr.length / 2);   //基准位置（理论上可任意选取）
+//     let pivot = arr.splice(pivotIndex, 1)[0];  //基准数   splice 会改改变源数组！！！！！！！！
+//     let left = [];
+//     let right = [];
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] < pivot) {
+//             left.push(arr[i]);
+//         } else {
+//             right.push(arr[i]);
+//         }
+//     }
+//     return quickSort(left).concat([pivot], quickSort(right));  //链接左数组、基准数构成的数组、右数组
+// };
+// console.log(quickSort(arrNumber.slice(0)));
+//
+// function selectionSort(arr) {
+//     let len = arr.length;
+//     let minIndex, temp;
+//     for (let i = 0; i < len - 1; i++) {
+//         minIndex = i;
+//         for (let j = i + 1; j < len; j++) {
+//             if (arr[j] < arr[minIndex]) {     // 寻找最小的数
+//                 minIndex = j;                 // 将最小数的索引保存
+//             }
+//         }
+//         temp = arr[i];
+//         arr[i] = arr[minIndex];
+//         arr[minIndex] = temp;
+//     }
+//     return arr;
+// }
+// console.log(selectionSort(arrNumber));
+//
+//
+// function insertionSort(arr) {
+//     let len = arr.length;
+//     let preIndex, current;
+//     for (let i = 1; i < len; i++) {
+//         preIndex = i - 1;
+//         current = arr[i];
+//         while(preIndex >= 0 && arr[preIndex] > current) {
+//             arr[preIndex+1] = arr[preIndex];
+//             preIndex--;
+//         }
+//         arr[preIndex+1] = current;
+//     }
+//     return arr;
+// }
+// console.log(insertionSort(arrNumber));
+//
+//
+//
+// function bubbleSort(arr) {
+//     let len = arr.length;
+//     for (let i = 0; i < len - 1; i++) {
+//         for (let j = 0; j < len - 1 - i; j++) {
+//             if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+//                 let temp = arr[j+1];        // 元素交换
+//                 arr[j+1] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+// console.log(bubbleSort(arrNumber));
 
-function selectionSort(arr) {
-    let len = arr.length;
-    let minIndex, temp;
-    for (let i = 0; i < len - 1; i++) {
-        minIndex = i;
-        for (let j = i + 1; j < len; j++) {
-            if (arr[j] < arr[minIndex]) {     // 寻找最小的数
-                minIndex = j;                 // 将最小数的索引保存
+
+// 希尔排序
+function shellSort(arr) {
+    var len = arr.length,
+        temp,
+        gap = 1;
+    while (gap < len / 3) {          // 动态定义间隔序列
+        gap = gap * 3 + 1;
+    }
+    for (gap; gap > 0; gap = Math.floor(gap / 3)) {
+        for (var i = gap; i < len; i++) {
+            temp = arr[i];
+            for (var j = i-gap; j > 0 && arr[j]> temp; j-=gap) {
+                arr[j + gap] = arr[j];
             }
-        }
-        temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
-    }
-    return arr;
-}
-console.log(selectionSort(arrNumber));
-
-
-function insertionSort(arr) {
-    let len = arr.length;
-    let preIndex, current;
-    for (let i = 1; i < len; i++) {
-        preIndex = i - 1;
-        current = arr[i];
-        while(preIndex >= 0 && arr[preIndex] > current) {
-            arr[preIndex+1] = arr[preIndex];
-            preIndex--;
-        }
-        arr[preIndex+1] = current;
-    }
-    return arr;
-}
-console.log(insertionSort(arrNumber));
-
-
-
-function bubbleSort(arr) {
-    let len = arr.length;
-    for (let i = 0; i < len - 1; i++) {
-        for (let j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
-                let temp = arr[j+1];        // 元素交换
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-            }
+            arr[j + gap] = temp;
         }
     }
     return arr;
 }
-console.log(bubbleSort(arrNumber));
+console.log(shellSort(arrNumber));
