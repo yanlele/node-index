cf2ac7fa9b186a050cee8ed51b6233ca99fe160a
yanlele <331393627@qq.com>
2018-12-08 10:25:49 +0800

    [+]添加一个高阶函数的使用示例

--- book/13、测试专题/01篇、javascript单元测试框架mochajs详解/01、最简单的一个例子.js
+++ book/13、测试专题/01篇、javascript单元测试框架mochajs详解/01、最简单的一个例子.js
@@ -13,6 +13,10 @@ describe('Array', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal(-1, [1, 2, 3].indexOf(4));
             console.log('it');
+        });
+
+        it('just is a console.log', function () {
+            console.log('123');
         })
 
         afterEach(function () {
