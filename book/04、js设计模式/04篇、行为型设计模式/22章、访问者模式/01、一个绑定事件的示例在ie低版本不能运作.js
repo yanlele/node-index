3f91391bb899e8a481843d18b8750b70bb02cfe1
yanlele <331393627@qq.com>
2018-12-22 20:44:29 +0800

    [+]添加一个实例

--- book/04、js设计模式/04篇、行为型设计模式/22章、访问者模式/01、一个绑定事件的示例在ie低版本不能运作.js
+++ book/04、js设计模式/04篇、行为型设计模式/22章、访问者模式/01、一个绑定事件的示例在ie低版本不能运作.js
@@ -2,3 +2,18 @@
  * create by yanlele
  * create time 2018-12-21 20:29
  */
+
+let bindEvent = function (dom, type, fn) {
+    if(dom.addEventListener) {
+        dom.addEventListener(type, fn, false);
+    } else if(dom.attachEvent) {
+        dom.attachEvent('on'+ type, fn);
+    } else {
+        dom['on'+ type] = fn;
+    }
+};
+
+let demo = document.getElementById('dome');
+bindEvent(demo, 'click', function () {
+    this.style.background = 'red';
+});
