21c3c4d0ed41e433c092441dfe20bbfcdf33cf55
yanlele <331393627@qq.com>
2019-01-03 19:17:26 +0800

    [+]解释器模式

---
+++ book/04、js设计模式/04篇、行为型设计模式/26、解释器模式/01、Interpreter.js
@@ -0,0 +1,18 @@
+/**
+ * create by yanle
+ * create time 2019/1/3 下午7:14
+ */
+
+class Interpreter {
+    // 获取兄弟元素名称
+    getSublingName(node) {
+        // 存在兄弟节点
+        if(node.previousSibling) {
+            let name = '',
+                count = 1,
+                nodeName = node.nodeName,
+                sibling = node.previousSibling;
+
+        }
+    }
+}
