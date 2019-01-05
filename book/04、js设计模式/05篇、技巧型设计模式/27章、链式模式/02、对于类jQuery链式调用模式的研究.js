307c310d37e4a2912e3462e287db10ec11f666eb
yanlele <331393627@qq.com>
2019-01-05 19:22:53 +0800

    [+]添加样式方法

--- book/04、js设计模式/05篇、技巧型设计模式/27章、链式模式/02、对于类jQuery链式调用模式的研究.js
+++ book/04、js设计模式/05篇、技巧型设计模式/27章、链式模式/02、对于类jQuery链式调用模式的研究.js
@@ -113,24 +113,24 @@ A.fn.extend({
         if (document.addEventListener) {
             return function (type, fn) {
                 let i = this.length - 1;
-                for (; i >= 0 ; i--) {
+                for (; i >= 0; i--) {
                     this[i].addEventListener(type, fn, false);
                 }
                 return this;
             }
-        } else if(document.attachEvent) {
+        } else if (document.attachEvent) {
             return function (type, fn) {
                 let i = this.length - 1;
                 for (; i >= 0; i--) {
-                    this[i].addEvent('on'+type, fn);
+                    this[i].addEvent('on' + type, fn);
                 }
                 return this;
             }
         } else {
             return function (type, fn) {
                 let i = this.length - 1;
-                for(;i >= 0;i--) {
-                    this[i]['on'+type] = fn;
+                for (; i >= 0; i--) {
+                    this[i]['on' + type] = fn;
                 }
                 return this;
             }
@@ -138,11 +138,43 @@ A.fn.extend({
     }
 });
 
-A.fn.extend({
+A.extend({
     // 将 '-' 分割线转为驼峰式： 'border-color' -> 'borderColor'
     camelCase: function (str) {
         return str.replace(/\-(\w)/g, function (all, letter) {
             return letter.toUpperCase();
         })
+    }
+});
+A.extend({
+    css: function () {
+        let args = arguments,
+            len = arg.length;
+        if (this.length < 1) {
+            return this;
+        }
+        // 只有一个参数的情况
+        if (len === 1) {
+            if (typeof args[0] === 'string') {
+                // IE
+                if (this[0].currentStyle) {
+                    return this[0].currentStyle[name];
+                } else {
+                    return getComputedStyle(this[0], false)[name];
+                }
+            } else if(typeof args[0] === 'object') {    // 为对象时则设置多个样式
+                for(let i in args[0]) {
+                    for (let j = this.length - 1; j >= 0; j--) {
+                        // 调用拓展方法 camelCase
+                        this[j].style[A.camelcase(i)] = args[0][i];
+                    }
+                }
+            }
+        }else if(len === 2) {     // 两个参数则设置一个样式
+            for(let j = this.length - 1; j>=0;j--) {
+                this[j].style[A.camelCase(args[0])] = args[1];
+            }
+        }
+        return this;
     }
 });
