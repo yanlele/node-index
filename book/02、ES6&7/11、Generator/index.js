41f09e80f936bb99674fda6004750bb98932da44
yanlele <331393627@qq.com>
2019-02-12 20:07:28 +0800

    add article

--- book/02、ES6&7/11、Generator/index.js
+++ book/02、ES6&7/11、Generator/index.js
@@ -5,13 +5,13 @@ function* helloWorldGenerator() {
 }
 var hw = helloWorldGenerator();
 
-hw.next()
+console.log(hw.next());
 // { value: 'hello', done: false }
-hw.next()
+console.log(hw.next());
 // { value: 'world', done: false }
-hw.next()
+hw.next();
 // { value: 'ending', done: true }
-hw.next()
+hw.next();
 // { value: undefined, done: true }
 
 
