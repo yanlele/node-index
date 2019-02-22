486b8b9bd9862a028c7dc4135d9817e2484bea96
yanlele <331393627@qq.com>
2019-02-22 18:02:46 +0800

    mock

--- 18年/6月/04、Object.assign() 与 深拷贝/index.js
+++ 18年/6月/04、Object.assign() 与 深拷贝/index.js
@@ -2,19 +2,11 @@ let Mock  = require('mockjs');
 
 let data = Mock.mock({
     'list|1-10': [{
-        'id|+1': Mock.mock('@integer(1,10)'),
-        name: Mock.mock('@cname'),
-        title: Mock.mock('@ctitle')
+        'id|+1': 1,
+        name: '@cname',
+        title: '@ctitle'
     }],
     message: '123'
 });
 
-data.prototype = {
-    name: 'yanle'
-};
-
-let reData = Object.assign({}, data);
-reData.message = 456;
-
-console.log('data', data.prototype.name);
-console.log('reData', reData);
+console.log('reData', data);
