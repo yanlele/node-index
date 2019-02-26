ac1e5a3411fbbfb2b158826826edf4c85a0a07ef
yanlele <331393627@qq.com>
2019-02-26 10:24:55 +0800

    promise 和 地狱回调的区别

--- 19年/02月/test/06、毁掉地狱就是这种.js
+++ 19年/02月/test/06、毁掉地狱就是这种.js
@@ -1,41 +1,45 @@
 const function1 = function (callback) {
-    setTimeout(callback, 1000)
+    setTimeout(function () {
+        callback(1);
+    }, 1000)
 };
 
 const function2 = function(callback) {
-    setTimeout(callback, 1000)
+    setTimeout(function () {
+        callback(2)
+    }, 1000)
 };
 
 const function3 = function(callback) {
-    setTimeout(callback, 1000)
+    setTimeout(function () {
+        callback(3)
+    }, 1000)
 };
 
 const function4 = function(callback) {
-    setTimeout(callback, 1000)
+    setTimeout(function () {
+        callback(4)
+    }, 1000)
 };
 
 const function5 = function(callback) {
-    setTimeout(callback, 1000)
+    setTimeout(function () {
+        callback(5)
+    }, 1000)
 };
 
-function1(function() {
-    console.log('function1');
-    function2(function () {
-        console.log('function2');
-        function3(function() {
-            console.log('function3执行');
-            function4(function () {
-                console.log('function4执行');
-                function5(function () {
-                    console.log('function5执行');
+function1(function(res1) {
+    console.log('function1', res1);
+    function2(function (res2) {
+        console.log('function2', res2);
+        function3(function(res3) {
+            console.log('function3执行', res3);
+            function4(function (res4) {
+                console.log('function4执行', res4);
+                function5(function (res5) {
+                    console.log('function5执行', res5);
                 })
             })
         })
     })
 });
-
-const buildPromise = function () {
-    return new Promise((resolve, reject) => {
-
-    })
-};
