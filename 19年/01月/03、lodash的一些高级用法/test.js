32da2799605057732bd86cefd5d40db922cbf26e
yanlele <331393627@qq.com>
2019-02-27 10:56:01 +0800

    add lodash demo

--- 19年/01月/03、lodash的一些高级用法/test.js
+++ 19年/01月/03、lodash的一些高级用法/test.js
@@ -1,8 +1,18 @@
 const _ = require('lodash');
 
 
-let object = {'a': 1, 'b': '2', 'c': 3};
+/*let object = {'a': 1, 'b': '2', 'c': 3};
 let result = _.pickBy(object, function (value, key) {
     return typeof value === "string"
 });
-console.log(result);    // { b: '2' }
+console.log(result);    // { b: '2' }*/
+
+console.log(_.isEmpty(true));
+
+
+const data = {
+    name: 'yanle',
+    age: 25
+};
+
+console.log(Object.entries(data));
