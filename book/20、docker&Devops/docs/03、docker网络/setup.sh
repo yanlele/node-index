46b38bef8d911b55bdfc0781bb057d8c202b1087
yanlele <331393627@qq.com>
2019-04-09 00:32:38 +0800

    [+]change setup.sh

--- book/20、docker&Devops/docs/03、docker网络/setup.sh
+++ book/20、docker&Devops/docs/03、docker网络/setup.sh
@@ -4,8 +4,10 @@
 sudo yum install -y git vim gcc glibc-static telnet bridge-utils
 
 # install docker
-curl -fsSL get.docker.com -o get-docker.sh
-sh get-docker.sh
+sudo yum remove docker  docker-common docker-selinux docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce-17.12.0.ce
 
 # start docker service
 sudo groupadd docker
--- book/20、docker&Devops/readme.md
+++ book/20、docker&Devops/readme.md
@@ -1,7 +1,11 @@
 ## docker&DevOps
 
+
+docker专题
 - [01、docker环境搭建](./docs/01、docker环境搭建)
 - [02、Docker容器与镜像](./docs/02、Docker容器与镜像)
 
 
 - [docker国内社区：http://dockone.io](http://dockone.io)
+- [https://docs.docker.com/](https://docs.docker.com/)
+- [https://hub.docker.com/](https://hub.docker.com/)
