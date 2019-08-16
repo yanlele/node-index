3a7d3a947e2065e21d10baca0e965b6ebd550379
yanlele <331393627@qq.com>
2019-08-16 17:57:03 +0800

    [+]try/catch 研究 和 mock 解决方案

--- 19年/08月/test/02、try&catch.js
+++ 19年/08月/test/02、try&catch.js
@@ -5,23 +5,22 @@ const myFunc = new Promise((resolve, reject) => {
 
     if (a + b === 4) {
       return resolve(a + b)
-    } else {
-      return reject('false')
     }
     throw new Error('错误了');
 
   } catch (e) {
-    console.log('error message', e.message);
-    resolve(e.message)
+    // console.log('error message', e.message);
+    resolve(e.message);
+    return e.message
   }
 });
 
-const result = myFunc;
-result.then((res)=> {
-  console.log(res);
-}).catch(()=> {
-  console.log('promise cache error');
-});
+// const result = myFunc;
+// result.then((res)=> {
+//   console.log(res);
+// }).catch(()=> {
+//   console.log('promise cache error');
+// });
 
 
 const resultFunction = async() => myFunc;
