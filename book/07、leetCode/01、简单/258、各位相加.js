b7b019f6232b7c3db3e87cdbcdd0731d0c0adff8
yanlele <331393627@qq.com>
2019-11-30 18:38:25 +0800

    feat: formt

--- book/07、leetCode/01、简单/258、各位相加.js
+++ book/07、leetCode/01、简单/258、各位相加.js
@@ -10,23 +10,23 @@
 * */
 
 /*用了循环和递归*/
-let addDigits = function(num) {
-    let arr = [];
-    for(let index of num.toString()) {
-        arr.push(index)
-    }
-    if(arr.length === 1) {
-        return parseInt(arr[0]);
-    }
-    let number = 0;
-    for(let arrLen =0;arrLen<arr.length;arrLen++) {
-        number += parseInt(arr[arrLen]);
-    }
-    if(number > 9) {
-        return addDigits(number)
-    } else {
-        return number;
-    }
+let addDigits = function (num) {
+  let arr = [];
+  for (let index of num.toString()) {
+    arr.push(index)
+  }
+  if (arr.length === 1) {
+    return parseInt(arr[0]);
+  }
+  let number = 0;
+  for (let arrLen = 0; arrLen < arr.length; arrLen++) {
+    number += parseInt(arr[arrLen]);
+  }
+  if (number > 9) {
+    return addDigits(number)
+  } else {
+    return number;
+  }
 };
 
 let res = addDigits(3118);
