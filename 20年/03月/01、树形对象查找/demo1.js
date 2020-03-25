2f0be25e745aa276a282d1d01721561c5d78010c
yanlele <331393627@qq.com>
2020-03-25 23:32:28 +0800

    feat: 深度遍历思想

--- 20年/03月/01、树形对象查找/demo1.js
+++ 20年/03月/01、树形对象查找/demo1.js
@@ -3,6 +3,27 @@
  * create time 2020-03-23 22:53
  */
 
-const { tree } = require('./data');
+const {tree} = require('./data');
 
 /* 深度遍历思想 */
+const deepQuery = (tree, id) => {
+  let isGet = false;
+  let retNode = null;
+  const deepSearch = (tree, id) => {
+    for (let index = 0; index < tree.length; index++) {
+      if (tree[index].children && tree[index].children.length > 0) {
+        deepSearch(tree[index].children, id);
+      }
+      if (id === tree[index].id || isGet) {
+        if (!isGet) retNode = tree[index];
+        isGet = true;
+        break;
+      }
+    }
+  };
+  deepSearch(tree, id);
+  return retNode;
+};
+
+const getNode = deepQuery(tree, '10102');
+console.log(getNode)
