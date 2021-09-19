7a00b3e88399e568275e2ae4b5dbf10cafde64a5
yanlele <331393627@qq.com>
2021-09-19 23:09:48 +0800

    feat: 链表结构

--- book/07、算法学习/其他/01、链表/index.ts
+++ book/07、算法学习/其他/01、链表/index.ts
@@ -9,11 +9,28 @@ export class Node {
     this.next = null;
     this.key = key;
   }
+
+
+  get _next(): any {
+    return this.next;
+  }
+
+  set _next(value: any) {
+    this.next = value;
+  }
+
+  get _key(): any {
+    return this.key;
+  }
+
+  set _key(value: any) {
+    this.key = value;
+  }
 }
 
 class NodeList {
   // 初始化节点
-  private head: null | Node;
+  private readonly head: null | Node;
   private length: number;
 
   constructor() {
@@ -24,5 +41,19 @@ class NodeList {
   // 创建节点
   static createNode(key) {
     return new Node(key);
+  }
+
+  // 插入数据
+  insert(node: Node) {
+    if (this.head) node._next(this.head);
+    else node._next(null);
+  }
+
+  find(key) {
+    let node = this.head;
+    while (node !== null && node._key !== key) {
+      node = node._next;
+    }
+    return node;
   }
 }
