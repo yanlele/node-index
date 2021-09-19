f024d03d9b04f318a90d80c931907eda652e9ced
yanlele <331393627@qq.com>
2021-09-19 21:34:42 +0800

    feat: 各位相加

--- book/07、算法学习/01、简单/258、各位相加.js
+++ book/07、算法学习/01、简单/258、各位相加.js
@@ -16,8 +16,8 @@
 /*用了循环和递归*/
 let addDigits = function (num) {
   let arr = [];
-  for (let index of num.toString()) {
-    arr.push(index)
+  for (let item of num.toString()) {
+    arr.push(item)
   }
   if (arr.length === 1) {
     return parseInt(arr[0]);
@@ -33,5 +33,19 @@ let addDigits = function (num) {
   }
 };
 
-let res = addDigits(3118);
-console.log(res);
+/**
+ * 解法思路2
+ * 我再给简单补充一下，数字：xyz=100*x+10*y+z=99*x+x+9*y+z=(99*x+9*y)+(x+y+z)，
+ * 也就是说xyz和（x+y+z）对于9来说是同模的, 即xyz%9 =（x+y+z）% 9，
+ * 这里假设（x+y+z）= ab，同理（ab%9=(a+b)%9），
+ * 可以一直递归下去直到一位数字，也就是说我们可以得到模9的结果，
+ * 但是我们需要的是模10的结果，num%10=((num-1)%9+1)%10=(num-1)%9+1,这里需要注意的是，1<=num<=9，然后写代码即可
+ * @param num
+ */
+let addDigits2 = function (num) {
+  if (0 === num % 9) return 9;
+  return num % 9;
+}
+
+console.log(addDigits(3118));
+console.log(addDigits2(3118));
