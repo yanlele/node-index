3248f83b1e6673e73f0819d11c63cb72112b602f
yanlele <331393627@qq.com>
2021-11-03 22:06:06 +0800

    feat: 删除不必要的文件

--- Test/demo/sqrt.js
+++
@@ -1,8 +0,0 @@
-var My = {
-    sqrt: function(x) {
-        if (x < 0) throw new Error("负值没有平方根");
-        return Math.exp(Math.log(x)/2);
-    }
-};
-
-module.exports = My;
--- Test/mockData/index.mock.js
+++
@@ -1,3 +0,0 @@
-let Mock = require('mockjs');
-export const time = new Date();
-
