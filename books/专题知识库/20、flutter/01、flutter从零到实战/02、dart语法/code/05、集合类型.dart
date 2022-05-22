cb2243b4fab7a444cb8a548f8b3362d87e381087
yanlele <331393627@qq.com>
2022-05-22 11:51:35 +0800

    feat: 集合

---
+++ books/专题知识库/20、flutter/01、flutter从零到实战/02、dart语法/code/05、集合类型.dart
@@ -0,0 +1,13 @@
+main(List<String> args) {
+  // 1. List
+  var names = ["abc", "bcd", "cde"];
+
+  // 2. Set
+  var movies = {"nihao", "nicai", "shibushi"};
+
+  // 3. map
+  var info = {
+    "name": "yanle",
+    "age": 30,
+  };
+}
