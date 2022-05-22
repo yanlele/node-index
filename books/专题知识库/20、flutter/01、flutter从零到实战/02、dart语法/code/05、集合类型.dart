26d1fcd4a5d5ee3f293e516e254ff226f7b66645
yanlele <331393627@qq.com>
2022-05-22 15:36:05 +0800

    feat: 集合的补充

--- books/专题知识库/20、flutter/01、flutter从零到实战/02、dart语法/code/05、集合类型.dart
+++ books/专题知识库/20、flutter/01、flutter从零到实战/02、dart语法/code/05、集合类型.dart
@@ -1,15 +1,18 @@
 main(List<String> args) {
   // 1. List
-  var names = ["abc", "bcd", "cde"];
+  var names = ["abc", "bcd", "cde", "abc"];
 
   // 2. Set
   var movies = {"nihao", "nicai", "shibushi"};
   // 去重
-  Set.from(names).toList();
+  names = Set<String>.from(names).toList();
+  print(names);
 
   // 3. map
   var info = {
     "name": "yanle",
     "age": 30,
   };
+
+  // 4. 没有关键字定义接口， 所有 class 都是隐式接口
 }
