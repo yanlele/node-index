e9e1eec9212f6601a9a7fa0b36e1f39fd6123b1c
yanlele <331393627@qq.com>
2022-05-23 22:49:50 +0800

    feat: inti

--- books/专题知识库/20、flutter/01、flutter从零到实战/03、HelloWorld/code/04_demo完整版.dart
+++ books/专题知识库/20、flutter/01、flutter从零到实战/03、HelloWorld/code/04_demo完整版.dart
@@ -1,64 +1,64 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: "demo home page"),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _count = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _count++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_count',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: "Increment",
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp(const MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "Flutter Demo",
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: "demo home page"),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _count = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _count++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_count',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: "Increment",
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
--- books/专题知识库/20、flutter/01、flutter从零到实战/03、HelloWorld/code/main.dart
+++ books/专题知识库/20、flutter/01、flutter从零到实战/03、HelloWorld/code/main.dart
@@ -1,98 +1,98 @@
-import 'package:flutter/material.dart';
-
-main() => runApp(const MyApp());
-
-/// Widget:
-///  有状态的 Widget: StateFulWidget 在运行时有一些状态是需要更改的
-///  无状态的 Widget: StatelessWidget 在运行时，无状态更改
-///  最终形成的一个 widget 数
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // 重写 build 方法
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "沙雕",
-      debugShowCheckedModeBanner: false,
-      // Scaffold 脚手架， 可以快速搭建页面
-      home: YLHomeBody(),
-    );
-  }
-}
-
-class YLHomeBody extends StatelessWidget {
-  // 必须要构造这个， 否则应用的时候会抛错
-  const YLHomeBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("title"),
-      ),
-      body: const YLContentBody(),
-    );
-  }
-}
-
-class YLContentBody extends StatelessWidget {
-  const YLContentBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: YLRow(),
-    );
-  }
-}
-
-class YLRow extends StatelessWidget {
-  const YLRow({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      // 主轴对齐方式
-      mainAxisAlignment: MainAxisAlignment.center,
-
-      // 子元素
-      children: const [
-        YLCheckBox(),
-        Text(
-          "hello",
-          style: TextStyle(fontSize: 20),
-        ),
-      ],
-    );
-  }
-}
-
-class YLCheckBox extends StatefulWidget {
-  const YLCheckBox({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return YLCheckBoxState();
-  }
-}
-
-
-/// 有一个状态概念
-/// 不能直接定义状态
-/// 创建一个单独的类， 来维护状态
-class YLCheckBoxState extends State<YLCheckBox> {
-  var _throwShotAway = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: _throwShotAway,
-      onChanged: (bool? newValue) {
-        setState(() {
-          _throwShotAway = newValue!;
-        });
-      },
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// main() => runApp(const MyApp());
+//
+// /// Widget:
+// ///  有状态的 Widget: StateFulWidget 在运行时有一些状态是需要更改的
+// ///  无状态的 Widget: StatelessWidget 在运行时，无状态更改
+// ///  最终形成的一个 widget 数
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   // 重写 build 方法
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: "沙雕",
+//       debugShowCheckedModeBanner: false,
+//       // Scaffold 脚手架， 可以快速搭建页面
+//       home: YLHomeBody(),
+//     );
+//   }
+// }
+//
+// class YLHomeBody extends StatelessWidget {
+//   // 必须要构造这个， 否则应用的时候会抛错
+//   const YLHomeBody({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("title"),
+//       ),
+//       body: const YLContentBody(),
+//     );
+//   }
+// }
+//
+// class YLContentBody extends StatelessWidget {
+//   const YLContentBody({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Center(
+//       child: YLRow(),
+//     );
+//   }
+// }
+//
+// class YLRow extends StatelessWidget {
+//   const YLRow({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       // 主轴对齐方式
+//       mainAxisAlignment: MainAxisAlignment.center,
+//
+//       // 子元素
+//       children: const [
+//         YLCheckBox(),
+//         Text(
+//           "hello",
+//           style: TextStyle(fontSize: 20),
+//         ),
+//       ],
+//     );
+//   }
+// }
+//
+// class YLCheckBox extends StatefulWidget {
+//   const YLCheckBox({Key? key}) : super(key: key);
+//
+//   @override
+//   State<StatefulWidget> createState() {
+//     return YLCheckBoxState();
+//   }
+// }
+//
+//
+// /// 有一个状态概念
+// /// 不能直接定义状态
+// /// 创建一个单独的类， 来维护状态
+// class YLCheckBoxState extends State<YLCheckBox> {
+//   var _throwShotAway = false;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Checkbox(
+//       value: _throwShotAway,
+//       onChanged: (bool? newValue) {
+//         setState(() {
+//           _throwShotAway = newValue!;
+//         });
+//       },
+//     );
+//   }
+// }
