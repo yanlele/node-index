845795b7ae0348d944b3ee8b47836ee3d8797715
yanlele <331393627@qq.com>
2023-11-26 00:52:16 +0800

    feat: 临时保存

---
+++ books/知识库/01、前端技术知识/27.请求超时重试/demo.js
@@ -0,0 +1,101 @@
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+var DEFAULT_TIMEOUT = 1000 * 1.5;
+var DEFAULT_RETRIES = [1000, 3 * 1000];
+var fetchWithRetries = function (url, initWithRetries) {
+    // fetchTimeout 请求超时时间
+    // 请求
+    var _a = initWithRetries || {}, fetchTimeout = _a.fetchTimeout, retryDelays = _a.retryDelays, init = __rest(_a, ["fetchTimeout", "retryDelays"]);
+    // 超时时间
+    var _fetchTimeout = fetchTimeout != null ? fetchTimeout : DEFAULT_TIMEOUT;
+    // 重复时间数组
+    var _retryDelays = retryDelays != null ? retryDelays : DEFAULT_RETRIES;
+    // 开始时间
+    var requestStartTime = 0;
+    // 重试请求索引
+    var requestsAttempted = 0;
+    return new Promise(function (resolve, reject) {
+        // 申明发送请求方法
+        var sendTimedRequest = function () {
+            // 自增索引与请求次数
+            requestsAttempted++;
+            // 发起请求时间
+            requestStartTime = Date.now();
+            // 是否需要处理后续请求
+            var isRequestAlive = true;
+            // 发起请求
+            var request = fetch(url, init);
+            // 请求超时情况
+            var requestTimeout = setTimeout(function () {
+                // 需要阻断正常的请求返回
+                isRequestAlive = false;
+                // 需要重新发起请求
+                if (shouldRetry(requestsAttempted)) {
+                    console.warn("fetchWithRetries: HTTP timeout, retrying.");
+                    retryRequest();
+                }
+                else {
+                    reject(new Error("fetchWithRetries(): Failed to get response from server, tried ".concat(requestsAttempted, " times.")));
+                }
+            }, _fetchTimeout);
+            // 正常请求发起
+            request.then(function (response) {
+                // 正常请求返回的场景， 清空定时器
+                clearTimeout(requestTimeout);
+                // 如果进入了超时流程， 那么正常返回的逻辑， 就直接阻断
+                if (isRequestAlive) {
+                    if (response.status >= 200 && response.status < 300) {
+                        resolve(response);
+                    }
+                    else if (shouldRetry(requestsAttempted)) {
+                        console.warn("fetchWithRetries: HTTP error, retrying.");
+                        retryRequest();
+                    }
+                    else {
+                        var error = new Error("response error.");
+                        error.response = response;
+                        reject(error);
+                    }
+                }
+            })["catch"](function (error) {
+                clearTimeout(requestTimeout);
+                if (shouldRetry(requestsAttempted)) {
+                    retryRequest();
+                }
+                else {
+                    reject(error);
+                }
+            });
+        };
+        // 发起重复请求
+        var retryRequest = function () {
+            // 重复请求 delay 时间
+            var retryDelay = _retryDelays[requestsAttempted - 1];
+            console.log("<".concat('='.repeat(50), "start").concat('='.repeat(50), ">"));
+            console.log('yanle - logger: _retryDelays', _retryDelays);
+            console.log('yanle - logger: requestsAttempted', requestsAttempted);
+            console.log('yanle - logger: retryDelay', retryDelay);
+            // 重复请求开始时间
+            var retryStartTime = requestStartTime + retryDelay;
+            console.log('yanle - logger: retryStartTime', retryStartTime);
+            console.log("<".concat('='.repeat(50), "end").concat('='.repeat(50), ">"));
+            // 重复请求
+            setTimeout(sendTimedRequest, retryStartTime - Date.now());
+        };
+        // 是否可以发起重复请求
+        var shouldRetry = function (attempt) { return attempt <= _retryDelays.length; };
+        sendTimedRequest();
+    });
+};
+fetchWithRetries("http://127.0.0.1:3000/user/").then(function (res) {
+    console.log("yanle - logger: res", res);
+});
--- books/知识库/01、前端技术知识/27.请求超时重试/demo.ts
+++ books/知识库/01、前端技术知识/27.请求超时重试/demo.ts
@@ -3,9 +3,8 @@ interface InitWithRetries extends RequestInit {
   retryDelays?: number[] | null;
 }
 
-const DEFAULT_TIMEOUT = 15000;
-const DEFAULT_RETRIES = [1000, 3000];
-
+const DEFAULT_TIMEOUT = 1000 * 1.5;
+const DEFAULT_RETRIES = [1000, 3 * 1000];
 
 const fetchWithRetries = (url: string, initWithRetries?: InitWithRetries): Promise<any> => {
   // fetchTimeout 请求超时时间
@@ -15,53 +14,102 @@ const fetchWithRetries = (url: string, initWithRetries?: InitWithRetries): Promi
   // 超时时间
   const _fetchTimeout = fetchTimeout != null ? fetchTimeout : DEFAULT_TIMEOUT;
 
+  // 重复时间数组
   const _retryDelays = retryDelays != null ? retryDelays : DEFAULT_RETRIES;
 
-  // 请求次数
-  let requestsAttempted = 0;
   // 开始时间
   let requestStartTime = 0;
 
-  // 申明发送请求方法
-  const sendTimedRequest = (): void => {
-    // 每次请求的时候， 请求次数自增
-    requestsAttempted++;
-
-    // 发起请求时间
-    requestStartTime = Date.now();
-
-    // 是否需要处理后续请求
-    let isRequestAlive = true;
-
-    // 发起请求
-    const request = fetch(url, init);
-
-    // 请求超时情况
-    const requestTimeout = setTimeout(() => {
-      //
-      isRequestAlive = false;
-
-      // 需要重新发起请求
-      if (shouldRetry(requestsAttempted)) {
-        console.warn("fetchWithRetries: HTTP timeout, retrying.");
-        retryRequest();
-      }
-    }, _fetchTimeout);
-  };
-
-  // 是否需要重试
-  const shouldRetry = (attempt: number): boolean => {
-    return attempt <= _retryDelays.length;
-  };
-
-  // 重新发起请求
-  const retryRequest = (): void => {
-    const retryDelay = _retryDelays[requestsAttempted - 1];
-    const retryStartTime = requestStartTime + retryDelay;
-    setTimeout(sendTimedRequest, retryStartTime - Date.now());
-  };
+  // 重试请求索引
+  let requestsAttempted = 0;
 
   return new Promise((resolve, reject) => {
+    // 申明发送请求方法
+    const sendTimedRequest = (): void => {
+      // 自增索引与请求次数
+      requestsAttempted++;
 
+      // 发起请求时间
+      requestStartTime = Date.now();
+
+      // 是否需要处理后续请求
+      let isRequestAlive = true;
+
+      // 发起请求
+      const request = fetch(url, init);
+
+      // 请求超时情况
+      const requestTimeout = setTimeout(() => {
+        // 需要阻断正常的请求返回
+        isRequestAlive = false;
+
+        // 需要重新发起请求
+        if (shouldRetry(requestsAttempted)) {
+          console.warn("fetchWithRetries: HTTP timeout, retrying.");
+          retryRequest();
+        } else {
+          reject(new Error(
+            `fetchWithRetries(): Failed to get response from server, tried ${requestsAttempted} times.`,
+          ));
+        }
+      }, _fetchTimeout);
+
+      // 正常请求发起
+      request.then(response => {
+        // 正常请求返回的场景， 清空定时器
+        clearTimeout(requestTimeout);
+
+        // 如果进入了超时流程， 那么正常返回的逻辑， 就直接阻断
+        if (isRequestAlive) {
+          if (response.status >= 200 && response.status < 300) {
+            resolve(response);
+          } else if (shouldRetry(requestsAttempted)) {
+            console.warn("fetchWithRetries: HTTP error, retrying.");
+            retryRequest();
+          } else {
+            const error: any = new Error(`response error.`);
+            error.response = response;
+            reject(error);
+          }
+        }
+      }).catch(error => {
+        clearTimeout(requestTimeout);
+        if (shouldRetry(requestsAttempted)) {
+          retryRequest();
+        } else {
+          reject(error);
+        }
+      });
+    };
+
+    // 发起重复请求
+    const retryRequest = (): void => {
+      // 重复请求 delay 时间
+      const retryDelay = _retryDelays[requestsAttempted - 1];
+
+      console.log(`<${'='.repeat(50)}start${'='.repeat(50)}>`);
+      console.log('yanle - logger: _retryDelays', _retryDelays);
+      console.log('yanle - logger: requestsAttempted', requestsAttempted);
+      console.log('yanle - logger: retryDelay', retryDelay);
+      
+
+      // 重复请求开始时间
+      const retryStartTime = requestStartTime + retryDelay;
+
+      console.log('yanle - logger: retryStartTime', retryStartTime);
+      console.log(`<${'='.repeat(50)}end${'='.repeat(50)}>`);
+
+      // 重复请求
+      setTimeout(sendTimedRequest, retryStartTime - Date.now());
+    };
+
+    // 是否可以发起重复请求
+    const shouldRetry = (attempt: number): boolean => attempt <= _retryDelays.length;
+
+    sendTimedRequest();
   });
 };
+
+fetchWithRetries("http://127.0.0.1:3000/user/").then(res => {
+  console.log("yanle - logger: res", res);
+});
